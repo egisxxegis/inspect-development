e2a584051b76914d3f8788d2bf9665b1ca78d63c
user0481 <egisxxegis@gmail.com>
2023-04-30 17:16:37 +0300

    add `asyncio.to_thread()` variant

--- sample_router.py
+++ sample_router.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Query
 from time import sleep
+import asyncio
 import logging
 
 router = APIRouter(
@@ -14,3 +15,8 @@ def sleepy(ms: int = Query(), id: str = Query()):
     print(f"will sleep {s} s. id = {id}")
     sleep(s)
     return id
+
+
+@router.get("/zzz/async")
+async def sleepy_async(ms: int = Query(), id: str = Query()):
+    return await asyncio.to_thread(sleepy, ms, id)
